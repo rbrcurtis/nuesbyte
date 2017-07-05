48782f7959074a508fe00549800a24433444cc8f
Ryan Curtis <wednesday@gmail.com>
2017-07-05 11:22:40 -0500

    getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  root: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
 
